1bd3f5cf4c9445b98c97c55e9deb782f50f99b21
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-02-01 11:38:23 +0300

    ok1

---
+++ Work1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] number = GetRandomArray(8);
+PrintArray(number);
+int count = 0;
+
+for (int i = 0; i < number.Length; i++)
+if (number[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"{number.Length} чисел, {count} из них чётные");
+
+int[] GetRandomArray(int length)
+{
+int[] result = new int[length];
+
+for (int i = 0; i < length; i++)
+{
+result[i] = new Random().Next(100, 1000);
+}
+return result;
+}
+
+void PrintArray(int[] arr)
+{
+foreach (int item in arr)
+{
+Console.Write(item + " ");
+}
+Console.WriteLine();
+}
